0c513ea66f016517a92d9d858d21e1dcbe456d7b
Naga <brunoyui@estudante.ufscar.br>
2024-07-10 23:52:50 -0300

    nAO TENHO CTZ
    AAAA

--- Assets/Scripts/Dialogo/DialogueInteraction.cs
+++ Assets/Scripts/Dialogo/DialogueInteraction.cs
@@ -21,7 +21,7 @@ public class DialogueInteraction : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && _canInteract) {
-            DialogueManager.OnStartDialogue?.Invoke(dialogues[0]);
+            DialogueManager.OnStartDialogue?.Invoke(0);
         }
     }
 }
