fbb26d04af5f1269ec7a9a15aed65695d40f4daf
Ast <93723066+YaboiAst@users.noreply.github.com>
2024-07-13 15:30:29 -0300

    Move Move

--- Assets/Scripts/Movimentação/Move.cs
+++ Assets/Scripts/Movimentação/Move.cs
@@ -7,11 +7,13 @@ public class Move : MonoBehaviour
 { 
     public SpriteRenderer spriteRenderer; 
     public Animator anim; 
-    public float speed;
-
+    public float speed; 
+    
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        DialogueManager.OnStartDialogue.AddListener((canmove) => this.enabled = false);
+        DialogueManager.OnFinishDialogue.AddListener(() => this.enabled = true);
     }
 
     void Update()
@@ -22,8 +24,9 @@ public class Move : MonoBehaviour
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.magnitude);
-        
+
         transform.position = transform.position + movement * (speed * Time.deltaTime);
+        
     }
     
 }
